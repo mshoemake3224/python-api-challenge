8396ce26ea86d4b7223abcb635de50a24a64f33d
mshoemake3224 <61944088+mshoemake3224@users.noreply.github.com>
2020-06-20 09:35:45 -0500

    HW Submission

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "6b9fca5bb7167ca8e527ee04dc1ced14"
+
+# Google API Key
+g_key = "AIzaSyAH-9gdqJfeNjSzrNX6rKkcs3tCSxl8-x0"
---
+++ WeatherPy/config.py
@@ -0,0 +1,1 @@
+api_key = "6ee84b31b054fd75aed30a2965bc8bd6"
